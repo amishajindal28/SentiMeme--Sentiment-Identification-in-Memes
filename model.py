a93c002c6c5d8904abd9fe2acd0bec043166a6ee
unknown <mario.azrs@gmail.com>
2021-04-20 22:46:46 -0400

    Add model change

--- model.py
+++ model.py
@@ -1,27 +1,17 @@
-#https://github.com/UKPLab/sentence-transformers
-from  sentence_transformers import SentenceTransformer
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-#Import data and pre trained model
-model = SentenceTransformer('distilbert-base-nli-mean-tokens')
-size_embeddings = 768
-
-data = pd.read_pickle("./data/labels_pd_pickle2")
-#TODO: Make this line run jeje
-not_working = []
-working = []
-for id_, sentence in enumerate(data["text_corrected"]):
-    try:
-        embed = model.encode(sentence)
-        working.append(embed)
-    except:
-        not_working.append((id_, sentence))
+#Import data 
+labels = np.load('./data/labels_num.npy')
+data = np.load('./data/text_embed.npy')
 
 #Shuffle data
-text_embed = text_embed[np.random.permutation(np.arange(len(text_embed)))]
-train_data, test_data = text_embed[:int(0.8*len(text_embed))], text_embed[:int(0.8*len(text_embed))]
+order = np.random.permutation(np.arange(len(data)))
+data, labels = data[order], labels[order]
+train_x, test_x = data[:int(len(data)*0.8)], data[int(len(data)*0.8):]
+train_y, test_y = labels[:int(len(data)*0.8)], labels[int(len(data)*0.8):]
+
 
 
 
